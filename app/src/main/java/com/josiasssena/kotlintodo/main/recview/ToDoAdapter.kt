51f1a2c0e2914baf89b9630e7ac80878af99408a
Josias Sena <josias1991@users.noreply.github.com>
2017-06-10 21:31:07 -0400

    Revert "Firebase"

--- app/src/main/java/com/josiasssena/kotlintodo/main/recview/ToDoAdapter.kt
+++ app/src/main/java/com/josiasssena/kotlintodo/main/recview/ToDoAdapter.kt
@@ -51,7 +51,7 @@ class ToDoAdapter : RecyclerView.Adapter<TodoViewHolder>() {
         AlertDialog.Builder(context)
                 .setView(body)
                 .setTitle(context.getString(R.string.edit_todo))
-                .setPositiveButton(context.getString(R.string.save), { _, _ ->
+                .setPositiveButton(context.getString(R.string.save), { dialog, which ->
                     realm.executeTransaction {
                         todo.title = etTitle.text.toString()
                         todo.body = etBody.text.toString()
@@ -66,13 +66,13 @@ class ToDoAdapter : RecyclerView.Adapter<TodoViewHolder>() {
                 .setCancelable(false)
                 .setTitle(context.getString(R.string.delete_dialog_titile))
                 .setMessage(context.getString(R.string.delete_dialog_body))
-                .setPositiveButton(android.R.string.ok, { _, _ ->
+                .setPositiveButton(android.R.string.ok, { dialog, which ->
                     realm.executeTransaction {
                         notifyItemRemoved(todoList.indexOf(todo))
                         todo.deleteFromRealm()
                     }
                 })
-                .setNegativeButton(android.R.string.cancel, { dialog, _ ->
+                .setNegativeButton(android.R.string.cancel, { dialog, which ->
                     dialog.dismiss()
                 })
                 .show()
